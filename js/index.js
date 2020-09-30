591d4af3a378170c11d475239ac5d0cae19fa2d8
Jake Grella <jgrella13@gmail.com>
2020-09-29 19:43:38 -0500

    completed 8 of 10 events

--- js/index.js
+++ js/index.js
@@ -3,7 +3,9 @@ const logo = document.querySelector('.logo-heading');
 const navLinks = document.querySelectorAll('.nav-link');
 const imgs = document.querySelectorAll('img');
 const footer = document.querySelector('.footer');
-const h2s = document.querySelectorAll('h2')
+const h2s = document.querySelectorAll('h2');
+const ps = document.querySelectorAll('p');
+const btns = document.querySelectorAll('.btn');
 
 // (mouseover) hovering over nav links turns them red
 navLinks.forEach((elem) => {
@@ -41,25 +43,57 @@ logo.addEventListener('click', (event) => {
 
 // (resize) when window resizes, change color of logo
 function logoFontColor() {
-    logo.style = 'color: green'
+	logo.style = 'color: green';
 }
-window.addEventListener('resize', logoFontColor)
+window.addEventListener('resize', logoFontColor);
 
 // (scroll) on scroll, change color of h2 randomly
 h2s.forEach((elem) => {
-    window.addEventListener('scroll', (event) => {
-        console.log('hi')
-        elem.style.color = `#${Math.floor(Math.random()*16777215).toString(16)}`
-    })
-})
+	window.addEventListener('scroll', (event) => {
+		elem.style.color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	});
+});
 
-// (keydown) escape key cancels all changes
+// doesn't work
+// (select)
+// function colorSelection(event) {
+//     console.log(event)
+//     ps.style.color = 'red'
+// }
+// window.addEventListener('select', colorSelection)
+
+// (dblclick) double click on logo decreases size
+logo.addEventListener('dblclick', (event) => {
+	logo.style = 'font-size: 2rem';
+});
+
+// (keyup) when spacebar is released, bottom buttons turn change to a random color
+document.addEventListener('keyup', function (event) {
+	if (event.key === ' ') {
+		btns.forEach((elem) => {
+			elem.style.backgroundColor = `#${Math.floor(
+				Math.random() * 16777215
+			).toString(16)}`;
+		});
+	}
+});
+
+// (auxclick) when right click or mouse wheel is clicked text turns red
+
+// document.addEventListener('auxclick')
+
+// (keydown) pressing escape key cancels all changes
 document.addEventListener('keydown', function (event) {
 	if (event.key === 'Escape') {
 		navLinks.forEach((elem) => {
 			elem.style.color = '#212529';
 		});
+		btns.forEach((elem) => {
+			elem.style.backgroundColor = '#17A2B8';
+        });
+        h2s.forEach((elem) => {
+			elem.style.color = '#000000';
+		});
 	}
-    logo.style = 'font-size: 4rem, color: #000000';
-
+	logo.style = 'font-size: 4rem; color: #000000';
 });
